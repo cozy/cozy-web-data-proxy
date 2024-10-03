88aea2be9daa0e5a737f370027b8750fdc583db7
Ldoppea <yannick.chiron@gmail.com>
2024-10-03 11:21:25 +0200

    feat: Cozy minimal setup

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,42 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginEjs } from "rsbuild-plugin-ejs";
+import { rspack } from "@rspack/core";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginEjs(), pluginReact()],
+  output: {
+    cleanDistPath: true,
+    distPath: {
+      root: 'build'
+    }
+  },
+  html: {
+    template: './src/targets/browser/index.ejs',
+    title: 'Cozy DataProxy'
+  },
+  source: {
+    entry: {
+      index: './src/targets/browser/index.tsx'
+    }
+  },
+  tools: {
+    rspack: {
+      plugins: [
+        new rspack.CopyRspackPlugin({
+          patterns: [
+            {
+              from: 'manifest.webapp',
+            },
+            {
+              from: 'README.md'
+            },
+            {
+              from: 'LICENSE'
+            }
+          ],
+        })
+      ]
+    }
+  }
 });

91702838c340313606c30fb479fb23a6ee0fa958
Ldoppea <yannick.chiron@gmail.com>
2024-10-13 15:22:57 +0200

    feat: Implement Search algorithm

--- src/dataproxy/worker/shared-worker.ts
+++ src/dataproxy/worker/shared-worker.ts
@@ -1,14 +1,22 @@
 import * as Comlink from 'comlink'
 
 import CozyClient from 'cozy-client'
+import Minilog from 'cozy-minilog'
 
 import {
   ClientData,
-  DataProxyWorker
+  DataProxyWorker,
+  SearchIndex
 } from '@/dataproxy/common/DataProxyInterface'
 import schema from '@/doctypes'
+import { initIndexes } from '@/search/initIndexes'
+import { search } from '@/search/search'
+
+const log = Minilog('👷‍♂️ [shared-worker]')
+Minilog.enable()
 
 let client: CozyClient | undefined = undefined
+let searchIndexes: SearchIndex[] | undefined = undefined
 
 const dataProxy: DataProxyWorker = {
   setClient: async (clientData: ClientData) => {
@@ -24,9 +32,24 @@ const dataProxy: DataProxyWorker = {
     })
     client.instanceOptions = clientData.instanceOptions
     client.capabilities = clientData.capabilities
+    if (!searchIndexes) {
+      const indexes = await initIndexes(client)
+      searchIndexes = indexes
+    }
   },
   search: async (query: string) => {
-    return 'Some Search Result'
+    log.debug('Received data for search')
+    if (!client) {
+      throw new Error(
+        'Client is required to execute a seach, please initialize CozyClient'
+      )
+    }
+
+    if (!searchIndexes) {
+      return []
+    }
+
+    return search(query, searchIndexes, client)
   }
 }
 
---
+++ src/search/consts.ts
@@ -0,0 +1,32 @@
+// Attribute order matters to apply priority on matching results
+export const SEARCH_SCHEMA = {
+  'io.cozy.files': ['name', 'path'],
+  'io.cozy.contacts': [
+    'displayName',
+    'fullname',
+    'email[]:address',
+    'address[]:formattedAddress',
+    'phone[]:number',
+    'cozy[]:url',
+    'birthday',
+    'company',
+    'jobTitle'
+  ],
+  'io.cozy.apps': ['slug', 'name']
+}
+
+export const FILES_DOCTYPE = 'io.cozy.files'
+export const CONTACTS_DOCTYPE = 'io.cozy.contacts'
+export const APPS_DOCTYPE = 'io.cozy.apps'
+
+export const TYPE_DIRECTORY = 'directory'
+export const ROOT_DIR_ID = 'io.cozy.files.root-dir'
+export const TRASH_DIR_ID = 'io.cozy.files.trash-dir'
+export const SHARED_DRIVES_DIR_ID = 'io.cozy.files.shared-drives-dir'
+
+export const LIMIT_DOCTYPE_SEARCH = 100
+export const DOCTYPE_ORDER = {
+  [APPS_DOCTYPE]: 0,
+  [CONTACTS_DOCTYPE]: 1,
+  [FILES_DOCTYPE]: 2
+}

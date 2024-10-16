97dac963d48427839683b2aee7537ec2a0dbded7
Ldoppea <yannick.chiron@gmail.com>
2024-10-16 15:36:09 +0200

    feat: Apply TS aliases to Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -24,5 +24,8 @@ export default {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tests/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^manifest.webapp$": ["<rootDir>/manifest.webapp"],
   },
+  resetMocks: true
 };

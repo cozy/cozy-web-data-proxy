0266111f7a1f761ece48db7a1ddbdd8debc30b1c
Paul Tran-Van <paul.tranvan@gmail.com>
2024-10-18 13:54:52 +0200

    chore: Make eslint less unpleasant

--- eslint.config.js
+++ eslint.config.js
@@ -101,13 +101,13 @@ const config = [
       '@typescript-eslint': tseslint.plugin
     },
     rules: {
-      '@typescript-eslint/no-unsafe-argument': 'error',
-      '@typescript-eslint/no-unsafe-assignment': 'error',
-      '@typescript-eslint/no-unsafe-call': 'error',
-      '@typescript-eslint/no-unsafe-member-access': 'error',
-      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
       '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { ignoreRestSiblings: true }

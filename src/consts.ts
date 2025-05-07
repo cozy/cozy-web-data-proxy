08a6eb59236289dd4a94b97de4a5d3d935bf5330
Paul Tran-Van <paul.tranvan@gmail.com>
2025-05-07 17:58:20 +0200

    feat: DataProxy can handle cozy-client requests
    `requestLink` is now exposed and allow to query the DataProxy's
    sharedWorker.
    Thanks to this, queries can be made locally by default, or remotely if
    the doctype is not locally persisted.
    The local databases are read-only, any write is made remotely and
    eventually replicated locally.

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,9 @@ export const REPLICATION_DEBOUNCE_MAX_DELAY = 600 * 1000 // 10min
 export const FILES_DOCTYPE = 'io.cozy.files'
 export const CONTACTS_DOCTYPE = 'io.cozy.contacts'
 export const APPS_DOCTYPE = 'io.cozy.apps'
+export const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
+export const KONNECTORS_DOCTYPE = 'io.cozy.konnectors'
+export const TRIGGERS_DOCTYPE = 'io.cozy.triggers'
+export const SETTINGS_DOCTYPE = 'io.cozy.settings'
 
 export const LOCALSTORAGE_KEY_DELETING_DATA = 'deletingLocalData'

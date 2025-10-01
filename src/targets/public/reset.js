42d3ed0139178cd50518540470fc23017a387525
Paul Tran-Van <paul.tranvan@gmail.com>
2025-10-01 15:39:04 +0200

    fix: Handle reset for old browser
    We rely on the `indexedDb.databases()` method to iterate over all local
    db to be removed, at login time. We also do it later, in the
    `SharedWorkerProvider` as a fallback.
    However, this does not work for old browsers that do not support this
    function.
    Therefore, we now store the db names locally in PouchLink, and rely on
    it to remove the databases.

--- src/targets/public/reset.js
+++ src/targets/public/reset.js
@@ -1,22 +1,92 @@
 /* eslint-disable no-console */
 
-function resetData() {
-  // Remove localstorage
-  window.localStorage.clear()
-  console.log('Deleted localStorage')
+const SHARED_WORKER_DB_NAME = 'sharedWorkerStorage'
+const STORE_NAME = 'store'
+const DB_NAMES_KEY = 'cozy-client-pouch-link-db-names'
 
-  // Add this flag for future check
-  window.localStorage.setItem('deletingLocalData', new Date().toISOString())
+function openDB(dbName) {
+  return new Promise(function (resolve, reject) {
+    const request = indexedDB.open(dbName, 1)
 
-  // Remove all indexedDB databases
-  window.indexedDB.databases().then(function (databases) {
-    databases.forEach(function (db) {
-      window.indexedDB.deleteDatabase(db.name)
-      console.log('Deleted indexedDB database : ', db.name)
-    })
-    // Everything is done, remove the flag
-    window.localStorage.removeItem('deletingLocalData')
+    request.onsuccess = function (event) {
+      const db = event.target.result
+      resolve(db)
+    }
+    request.onerror = function (event) {
+      reject(event.target.error)
+    }
   })
 }
 
-resetData()
+async function getItem(LOCAL_DB_NAME, key) {
+  const db = await openDB(LOCAL_DB_NAME)
+  if (!db) {
+    return null
+  }
+
+  return new Promise(function (resolve, reject) {
+    try {
+      const transaction = db.transaction(STORE_NAME, 'readonly')
+      const store = transaction.objectStore(STORE_NAME)
+      const request = store.get(key)
+
+      request.onsuccess = function () {
+        resolve(request.result ? request.result.value : null)
+      }
+
+      request.onerror = function () {
+        reject(request.error)
+      }
+    } catch (err) {
+      if (err.name === 'NotFoundError') {
+        console.log(`No db ${LOCAL_DB_NAME} found: nothing to clean`)
+        resolve(null)
+      }
+      reject(err)
+    }
+  })
+}
+
+;(async function () {
+  try {
+    // Remove localstorage
+    window.localStorage.clear()
+    console.log('Deleted localStorage.')
+
+    // Add this flag for future check
+    window.localStorage.setItem('deletingLocalData', new Date().toISOString())
+
+    if (typeof window.indexedDB?.databases === 'function') {
+      // Remove all indexedDB databases
+      window.indexedDB.databases().then(function (databases) {
+        databases.forEach(function (db) {
+          window.indexedDB.deleteDatabase(db.name)
+          console.log('Deleted indexedDB database: ', db.name)
+        })
+      })
+    } else {
+      // indexedDB.databases() does not exist: fallback to stored db names
+      const item = await getItem(SHARED_WORKER_DB_NAME, DB_NAMES_KEY)
+      if (!item) {
+        console.log('No databases info found. Nothing will be removed.')
+        return
+      }
+      const dbNames = JSON.parse(item)
+
+      dbNames.forEach(function (dbName) {
+        const request = window.indexedDB.deleteDatabase(dbName)
+        request.onsuccess = function () {
+          console.log('Deleted database:', dbName)
+        }
+        request.onerror = function (e) {
+          console.warn('Failed to delete database:', dbName, e)
+        }
+      })
+      window.indexedDB.deleteDatabase(LOCAL_DB_NAME)
+    }
+    // Everything is done, remove the flag
+    window.localStorage.removeItem('deletingLocalData')
+  } catch (e) {
+    console.log('Something went wrong during cleanup: ', e)
+  }
+})()
